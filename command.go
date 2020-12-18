f08d9503b23152bb1ab062e2ab5adb35a3b9d0cc
Shunta Komatsu <shuntak217@gmail.com>
2020-12-18 18:11:36 +0900

    fix: do not run commands line-by-line

--- command.go
+++ command.go
@@ -1,26 +1,9 @@
 package vin
 
 import (
-	"fmt"
 	"os/exec"
-	"strings"
 )
 
 func (a *App) RunCommand() error {
-	cmds := strings.Split(a.Command, "\n")
-	for _, cmd := range cmds {
-		if cmd == "" {
-			continue
-		}
-
-		out, err := exec.Command("/bin/sh", "-c", cmd).Output() //nolint:gosec
-		if err != nil {
-			return err
-		}
-
-		if o := string(out); o != "" {
-			fmt.Println(o)
-		}
-	}
-	return nil
+	return exec.Command("/bin/sh", "-c", a.Command).Run() //nolint:gosec
 }
